0300b01a0405138723cc36b7fc1c3f8b906a301c
khegi <khegi@khegiw>
2014-02-24 02:55:15 +0700

    commit project baru

---
+++ ci/ci-main/src/main/java/itb/rpl/ppl/tugas/ci/main/App.java
@@ -0,0 +1,13 @@
+package itb.rpl.ppl.tugas.ci.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
