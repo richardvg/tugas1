318ba9ce4d0dcc9dd08563916d39bb221b892ba5
khegi <khegi@khegiw>
2014-02-24 14:12:46 +0700

    tukar class A, untuk testing commit

--- ci/ci-main/src/main/java/itb/rpl/ppl/tugas/ci/main/A.java
+++ ci/ci-main/src/main/java/itb/rpl/ppl/tugas/ci/main/A.java
@@ -6,11 +6,13 @@ package itb.rpl.ppl.tugas.ci.main;
  * @author khegi
  */
 public class A {
-    public A() {
-		System.out.println("ctor-A");
-	}
-	
-	public void f() {
-		System.out.println("A");
-	}
+    public A(){
+     System.out.println("test");
+    
+    }
+    
+    public Void f(){
+        return null;
+    
+    }
 }
