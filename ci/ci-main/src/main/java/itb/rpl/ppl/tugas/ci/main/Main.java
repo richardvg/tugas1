59a3f0127ee527bd5346476c03e1c659d3193175
ASUS <ASUS@Aston>
2014-02-24 18:21:31 +0700

    update main

--- ci/ci-main/src/main/java/itb/rpl/ppl/tugas/ci/main/Main.java
+++ ci/ci-main/src/main/java/itb/rpl/ppl/tugas/ci/main/Main.java
@@ -1,16 +1,28 @@
 package itb.rpl.ppl.tugas.ci.main;
 
+import java.util.Vector;
+
 /**
  *test
  * @author khegi
  */
-public class Main {
+public class Main implements X{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+		ABB a = new ABB();
+		
+		Q q = new Q();
+		
+		a.f();
+		
+		System.out.println("Isi Vector:");
+		}
+
+	@Override
+	public int w() {
+		return 2;
+	}
 }
